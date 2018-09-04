f4e8b37bb3611a49f6cbf7df2a88ffe4ffb71683
Lisa Reinhardt <reinal@gmail.com>
2018-09-03 19:15:23 -0500

    updated main.js

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Switch, Route} from 'react-router-dom';
 import Home from './Home';
-import AddContact from './AddContact';
+import AddContact from './AddContactForm';
 import LandingPage from './LandingPage';
 import ContactList from './ContactList';
 import NewInteractionForm from './NewInteractionForm';
