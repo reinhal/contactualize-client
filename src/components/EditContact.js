cd1a7479912b89ef56daaec2b3bda4f2a9c98cac
Lisa Reinhardt <reinal@gmail.com>
2018-09-11 16:57:23 -0500

    updated editContact

--- src/components/EditContact.js
+++ src/components/EditContact.js
@@ -1,18 +1,24 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+import EditContactForm from './EditContactForm';
 import './styles/AddContactForm.css';
 
 
 export default function EditContact() {
   return (
-    <div>
-      <button 
-        className ="edit-contact" 
-        type="submit" 
-        data-a11y-dialog-hide aria-label="Edit this contact.">
-        <Link to="/edit-contact"><i className="far fa-edit"></i>
-         Edit</Link>
-      </button>
-    </div>
+    <Router>
+      <div>
+        <button 
+          className ="edit-contact" 
+          type="submit" 
+          data-a11y-dialog-hide aria-label="Edit this contact.">
+          <Route path="/contacts/this.props.contactID" component={EditContactForm} />
+          <i className="far fa-edit"></i>
+          Edit
+        </button>
+      </div>
+    </Router> 
   );
 }
+//react router
